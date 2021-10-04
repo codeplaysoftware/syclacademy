509c8b8e25f28817557296af3c76497ca860b569
Hugh Delaney <46290137+hdelan@users.noreply.github.com>
2021-10-04 10:23:37 +0100

    Update solution.cpp
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update reference.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp
    
    Update solution.cpp

--- Code_Exercises/Exercise_14_ND_Range_Kernel/solution.cpp
+++ Code_Exercises/Exercise_14_ND_Range_Kernel/solution.cpp
@@ -11,7 +11,11 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+#if __has_include(<SYCL/sycl.hpp>)
+#include <SYCL/sycl.hpp>
+#else
 #include <CL/sycl.hpp>
+#endif
 
 class vector_add_1;
 class vector_add_2;
