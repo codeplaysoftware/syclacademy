30500d71dbe322ce0a28235ae22e6671a23c73d7
Hugh Delaney <hugh.delaney@codeplay.com>
2022-02-28 18:19:27 +0000

    Adding solution5

--- Code_Exercises/solutions/ex5_solution.cpp
+++ Code_Exercises/solutions/ex5_solution.cpp
@@ -1,35 +1,33 @@
-/* 
+/*
  * This code performs a reduce operation over an array.
  * We use two separate kernels, in order to achieve a global
  * device synchronization.
- * 
+ *
  * Kernel 1:
  * Reduce the array into a smaller array of dimension num_wkgroups
- * 
+ *
  * Kernel 2:
- * Reduce the smaller array into a single value 
- * 
+ * Reduce the smaller array into a single value
+ *
  * Use optimization principles from slides in order to make code run faster.
- * 
+ *
  */
- 
 
 #include <CL/sycl.hpp>
 
+#define MIN(a, b) a < b ? a : b
+
 using T = float;
 
-constexpr size_t dataSize = 16'384;
-constexpr size_t wkgrp_sz =
-    32; // TODO check which wkgrp_sz is optimal for this problem
+constexpr size_t dataSize = 32'768;
+constexpr size_t wkgrp_sz = 1024; // OPTIMIZATION: Try different workgroup sizes
 constexpr size_t num_wkgrps =
-    2; // TODO does 2 active workgroups seem like an efficient use of hardware?
+    MIN(wkgrp_sz, dataSize / wkgrp_sz); // OPTIMIZATION: Use more workgroups
 
 /* ---------------------------------------------------------------------------*/
 /**
  *  \brief:      A function that reduces an array in local memory into a single
  *               value at a[0].
- *
- *               TODO what simple keyword may improve performance?
  *
  * \param:       a        Local accessor of dimension wkgrp_sz
  * \param:       item
@@ -37,11 +35,11 @@ constexpr size_t num_wkgrps =
  */
 /* ---------------------------------------------------------------------------*/
 template <typename T, size_t wkgrp_sz>
-void workgroup_reduce_local_mem(
-    sycl::accessor<T, 1, sycl::access::mode::read_write,
-                   sycl::access::target::local>
-        a,
-    sycl::nd_item<1> &item, size_t localIdx) {
+inline void // OPTIMIZATION: using inline
+workgroup_reduce_local_mem(sycl::accessor<T, 1, sycl::access::mode::read_write,
+                                          sycl::access::target::local>
+                               a,
+                           sycl::nd_item<1> &item, size_t localIdx) {
 #pragma unroll
   for (auto i = 1024; i >= 2; i /= 2) {
     if (wkgrp_sz >= i) {
@@ -63,7 +61,8 @@ int main() {
   auto q = sycl::queue{};
 
   T *devPtrA = sycl::malloc_device<T>(dataSize, q);
-  T *devPtrB = sycl::malloc_device<T>(num_wkgrps, q);
+  T *devPtrB =
+      sycl::malloc_device<T>(num_wkgrps, q); // Holds intermediate values
 
   q.memcpy(devPtrA, a, sizeof(T) * dataSize).wait();
 
@@ -71,13 +70,10 @@ int main() {
       sycl::nd_range(sycl::range(num_wkgrps * wkgrp_sz), sycl::range(wkgrp_sz));
 
   q.submit([&](sycl::handler &cgh) {
-     // Declaring a local accessor
      sycl::accessor<T, 1, sycl::access_mode::read_write,
                     sycl::access::target::local>
          local_mem(wkgrp_sz, cgh);
 
-     // Kernel to reduce array into a smaller array of
-     // dimension <num_wkgroups>
      cgh.parallel_for(my_nd, [=](sycl::nd_item<1> item) {
        auto localIdx = item.get_local_linear_id();
        auto globalIdx = item.get_global_linear_id();
@@ -85,41 +81,59 @@ int main() {
 
        local_mem[localIdx] = 0;
 
-       auto num_to_reduce_per_work_item =
-           dataSize / globalRange; // This must divide
-
-       auto array_idx = globalIdx * num_to_reduce_per_work_item;
+       auto arrayIdx = globalIdx;
 
        // Accumulating thread local reductions into local memory
-       // TODO what does this memory access pattern look like? Are adjacent
-       // threads accessing adjacent spaces in memory?
-       while (array_idx < (globalIdx + 1) * num_to_reduce_per_work_item) {
-         local_mem[localIdx] += devPtrA[array_idx++];
+       // OPTIMIZATION: coalesced memory access
+       while (arrayIdx < dataSize) {
+         local_mem[localIdx] += devPtrA[arrayIdx];
+         arrayIdx += globalRange;
        }
        item.barrier();
        workgroup_reduce_local_mem<T, wkgrp_sz>(local_mem, item, localIdx);
        devPtrB[item.get_group_linear_id()] = local_mem[0];
      });
    }).wait();
+  
+  // OPTIMIZATION: Using parallel_for instead of single_task
+  q.submit([&](sycl::handler &cgh) {
+     // Declaring a local accessor
+     sycl::accessor<T, 1, sycl::access_mode::read_write,
+                    sycl::access::target::local>
+         local_mem(wkgrp_sz, cgh);
 
-  // Kernel to reduce smaller array into final value at devPtrA[0]
-  // TODO if num_wkgrps is large, could we do this with a parallel_for?
-  q.single_task([=]() {
-     for (auto i = 1; i < num_wkgrps; i++)
-       devPtrB[0] += devPtrB[i];
+     // Second kernel executed by a single work group
+     auto second_nd =
+         sycl::nd_range(sycl::range(wkgrp_sz), sycl::range(wkgrp_sz));
+     
+     // Reduce smaller array into a single value
+     cgh.parallel_for(second_nd, [=](sycl::nd_item<1> item) {
+       auto localIdx = item.get_local_linear_id();
+       local_mem[localIdx] = 0;
+
+       auto arrayIdx = localIdx;
+       // Accumulating thread local reductions into local memory
+       while (arrayIdx < num_wkgrps) {
+         local_mem[localIdx] += devPtrB[arrayIdx];
+         arrayIdx += wkgrp_sz;
+       }
+       item.barrier();
+       workgroup_reduce_local_mem<T, wkgrp_sz>(local_mem, item, localIdx);
+       devPtrB[0] = local_mem[0];
+     });
    }).wait();
 
   T devAns = 0;
   q.memcpy(&devAns, devPtrB, sizeof(T));
-  
+
   T serialAns = 0;
-  for (auto i=0;i<dataSize;i++) {
+  for (auto i = 0; i < dataSize; i++) {
     serialAns += a[i];
   }
 
   std::cout << "Got device ans " << devAns << '\n';
   std::cout << "vs serial ans " << serialAns << '\n';
-  
+
   sycl::free(devPtrA, q);
   sycl::free(devPtrB, q);
 }
