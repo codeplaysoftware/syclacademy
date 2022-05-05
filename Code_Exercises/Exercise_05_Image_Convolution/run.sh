58f48aef0cfd3b31739ed8c450901b9310bac047
Rod Burns <rod@codeplay.com>
2022-05-05 18:07:55 +0100

    Adding include paths to command

--- Code_Exercises/Exercise_05_Image_Convolution/run.sh
+++ Code_Exercises/Exercise_05_Image_Convolution/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 /bin/echo "##" $(whoami) is compiling
-dpcpp -o image-convolution ./reference.cpp
+dpcpp -o image-convolution -I../../External/Catch2/single_include -I../../Utilities/include -I../../External/stb/ ./reference.cpp
 if [ $? -eq 0 ]; then ./image-convolution; fi
