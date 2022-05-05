f51a13688c73dd57c84471e2c5311cdf4bcc3566
Rod Burns <rod@codeplay.com>
2022-05-05 21:18:47 +0100

    Updating scripts for running in DevCloud

--- Code_Exercises/Exercise_03_Scalar_Add/run.sh
+++ Code_Exercises/Exercise_03_Scalar_Add/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 /bin/echo "##" $(whoami) is compiling
-dpcpp -o scalar-add ./source.cpp
-if [ $? -eq 0 ]; then ./scalar-add; fi
+dpcpp solution.cpp
+if [ $? -eq 0 ]; then ./a.out; fi
+
