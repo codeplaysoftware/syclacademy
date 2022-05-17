ef2d3c4101924312533dc0d31c1317cc5f95b982
Rod Burns <rod@codeplay.com>
2022-05-17 16:17:25 +0100

    Updating numbers of folders, adding Image content back

---
+++ Code_Exercises/Exercise_05_Image_Convolution/README.md
@@ -0,0 +1,66 @@
+# SYCL Academy
+
+## Exercise 15: Image Convolution
+---
+
+In this exercise there is no task, simply familiarize yourself with the image
+convolution reference code, as this will be used in later exercises.
+
+---
+
+### 1.) Reference image
+
+For the purposes of this exercise we have provided an image in the exercise
+directory called "dogs.png", however feel free to replace this with any other
+32bit PNG image, though note that this exercise will work best with images
+where the dimensions are multiples of 2, such as 512x512.
+
+Note you will have to update the path to an image. There is the image in the
+repository but feel free to use any image you choose. Though it's recommend that
+you use a png image whose dimensions are multiple of 2 (for example 512x512)
+and has four channels (RGBA).
+
+### 2.) Stb library
+
+The source for this example provides a stub which loads and write an image using
+the STB image library.
+
+### 3.) Benchmarking
+
+The source also contains a call to a benchmarking utility that will print the
+time taken to execute the SYCL code, the SYCL code should go inside the lambda
+that is passed to the `benchmark` function.
+
+Though note that the benchmark facility provided measures whole application time
+which is less accurate than measuring the kernel execution times alone.
+
+Try running the application and recording the benchmark result timing you see so
+you can compare this with results in later exercises.
+
+Note if you are running on the host device the default iterations for the
+benchmark of 100 will take a while to execute so try reducing this number.
+
+### 4.) Dimensionality
+
+The reference code uses a 2-dimensional `range` in `parallel_for` as this often
+simplifies the code when working with images.
+
+### 5.) Convolution filters
+
+The image convolution support code provides a `filter_type` enum which allows
+you to choose between `identity` and `blur`. The utility for generating the
+filter data; `generate_filter` takes a `filter_type` and a width.
+
+#### Build And Execution Hints
+
+In Intel DevCloud, to run computational applications, you will submit jobs to a queue for execution on compute nodes,
+especially some features like longer walltime and multi-node computation is only abvailable through the job queue.
+There is a script provided in the folder for this exercise to compile and run your code. 
+
+```sh
+qsub -l nodes=1:gpu:ppn=2 -d . run.sh
+```
+Once the job has finished, in the same folder will be a file with a name similar to `run.sh.o1898955`
+If you open this file you will see the program output from your SYCL code.
+
+Refer to the [guide][devcloud-job-submission] for further information on the possibilities.
---
+++ Code_Exercises/Exercise_05_Image_Convolution/reference.cpp
@@ -0,0 +1,129 @@
+/*
+ SYCL Academy (c)
+
+ SYCL Academy is licensed under a Creative Commons
+ Attribution-ShareAlike 4.0 International License.
+
+ You should have received a copy of the license along with this
+ work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+*/
+
+#include <algorithm>
+#include <iostream>
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <benchmark.h>
+#include <image_conv.h>
+
+#if __has_include(<SYCL/sycl.hpp>)
+#include <SYCL/sycl.hpp>
+#else
+#include <CL/sycl.hpp>
+#endif
+
+class image_convolution;
+
+inline constexpr util::filter_type filterType = util::filter_type::blur;
+inline constexpr int filterWidth = 11;
+inline constexpr int halo = filterWidth / 2;
+
+TEST_CASE("image_convolution_naive", "image_convolution_reference") {
+  const char* inputImageFile =
+      "../Images/dogs.png";
+  const char* outputImageFile =
+      "../Images/blurred_dogs.png";
+
+  auto inputImage = util::read_image(inputImageFile, halo);
+
+  auto outputImage = util::allocate_image(
+      inputImage.width(), inputImage.height(), inputImage.channels());
+
+  auto filter = util::generate_filter(util::filter_type::blur, filterWidth);
+
+  try {
+    sycl::queue myQueue{sycl::gpu_selector{},
+                        [](sycl::exception_list exceptionList) {
+                          for (auto e : exceptionList) {
+                            std::rethrow_exception(e);
+                          }
+                        }};
+
+    std::cout << "Running on "
+              << myQueue.get_device().get_info<sycl::info::device::name>()
+              << "\n";
+
+    auto inputImgWidth = inputImage.width();
+    auto inputImgHeight = inputImage.height();
+    auto channels = inputImage.channels();
+    auto filterWidth = filter.width();
+    auto halo = filter.half_width();
+
+    auto globalRange = sycl::range(inputImgWidth, inputImgHeight);
+    auto localRange = sycl::range(32, 1);
+    auto ndRange = sycl::nd_range(globalRange, localRange);
+
+    auto inBufRange = (inputImgWidth + (halo * 2)) * sycl::range(1, channels);
+    auto outBufRange = inputImgHeight * sycl::range(1, channels);
+    auto filterRange = filterWidth * sycl::range(1, channels);
+
+    {
+      auto inBuf = sycl::buffer{inputImage.data(), inBufRange};
+      auto outBuf = sycl::buffer<float, 2>{outBufRange};
+      auto filterBuf = sycl::buffer{filter.data(), filterRange};
+      outBuf.set_final_data(outputImage.data());
+
+      util::benchmark(
+          [&]() {
+            myQueue.submit([&](sycl::handler& cgh) {
+              auto inputAcc = inBuf.get_access<sycl::access::mode::read>(cgh);
+              auto outputAcc =
+                  outBuf.get_access<sycl::access::mode::write>(cgh);
+              auto filterAcc =
+                  filterBuf.get_access<sycl::access::mode::read>(cgh);
+
+              cgh.parallel_for<image_convolution>(
+                  ndRange, [=](sycl::nd_item<2> item) {
+                    auto globalId = item.get_global_id();
+
+                    auto channelsStride = sycl::range(1, channels);
+                    auto haloOffset = sycl::id(halo, halo);
+                    auto src = (globalId + haloOffset) * channelsStride;
+                    auto dest = globalId * channelsStride;
+
+                    float sum[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+                    for (int r = 0; r < filterWidth; ++r) {
+                      for (int c = 0; c < filterWidth; ++c) {
+                        auto srcOffset =
+                            sycl::id(src[0] + (r - halo),
+                                     src[1] + ((c - halo) * channels));
+                        auto filterOffset = sycl::id(r, c * channels);
+
+                        for (int i = 0; i < 4; ++i) {
+                          auto channelOffset = sycl::id(0, i);
+                          sum[i] += inputAcc[srcOffset + channelOffset] *
+                                    filterAcc[filterOffset + channelOffset];
+                        }
+                      }
+                    }
+
+                    for (size_t i = 0; i < 4; ++i) {
+                      outputAcc[dest + sycl::id{0, i}] = sum[i];
+                    }
+                  });
+            });
+
+            myQueue.wait_and_throw();
+          },
+          100, "image convolution (coalesced)");
+    }
+  } catch (sycl::exception e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
+
+  util::write_image(outputImage, outputImageFile);
+
+  REQUIRE(true);
+}
