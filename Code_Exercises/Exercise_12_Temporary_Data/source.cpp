bc90e502871a0512458f814d115a5e7cbec8c0b5
Hugh Delaney <hugh.delaney@codeplay.com>
2022-08-26 15:51:39 +0100

    Add some more SYCL quick refs

--- Code_Exercises/Exercise_12_Temporary_Data/source.cpp
+++ Code_Exercises/Exercise_12_Temporary_Data/source.cpp
@@ -6,6 +6,45 @@
 
  You should have received a copy of the license along with this
  work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+
+ * SYCL Quick Reference
+ * ~~~~~~~~~~~~~~~~~~~~
+ *
+ * // Default construct a queue
+ * auto q = sycl::queue{};
+ *
+ * // Declare a buffer pointing to ptr
+ * auto buf = sycl::buffer{ptr, sycl::range{n}};
+ *
+ * // Declare a buffer using host ptr
+ * auto buf = sycl::buffer{ptr, sycl::range{n}, 
+ *                 {sycl::property::buffer::use_host_ptr{}}};
+ *
+ * // Declare a buffer relating to no host memory
+ * auto buf = sycl::buffer{sycl::range{n}};
+ *
+ * // Set final data of a buffer
+ * buf.set_final_data(host_ptr);
+ *
+ * // Set final data of a buffer to nullptr
+ * buf.set_final_data(nullptr);
+ *
+ * // Submit work to the queue
+ * q.submit([&](sycl::handler &cgh) {
+ *   // COMMAND GROUP
+ * });
+ *
+ * // Within the command group you can
+ * //    1. Declare an accessor to a buffer
+ *          auto read_write_acc = sycl::accessor{buf, cgh};
+ *          auto read_acc = sycl::accessor{buf, cgh, sycl::read_only};
+ *          auto write_acc = sycl::accessor{buf, cgh, sycl::write_only};
+ *          auto no_init_acc = sycl::accessor{buf, cgh, sycl::no_init};
+ * //    2. Enqueue a parallel for:
+ *          cgh.parallel_for<class mykernel>(sycl::range{n}, [=](sycl::id<1> i) {
+ *              // Do something
+ *          });
+ *
 */
 
 #define CATCH_CONFIG_MAIN
