e10046cec7892149be160139c58911a6bffe4ef1
Hugh Delaney <hugh.delaney@codeplay.com>
2022-10-26 16:21:03 +0100

    simplify solution

--- Code_Exercises/Exercise_08_Matrix_Transpose/solution.cpp
+++ Code_Exercises/Exercise_08_Matrix_Transpose/solution.cpp
@@ -6,6 +6,41 @@
 
  You should have received a copy of the license along with this
  work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+
+ The tiling should work as follows:
+
+ The groupOffset will need to be inverted, as well as the localId.
+
+ In:                                      Out:
+ +----------------------------+           +----------------------------+
+ |             group  ^       |           |       ^                    |
+ |           Offset[1]|       |           |       |                    |
+ |                    V       |           |       |                    |
+ |<------------------>+-------+           |       |                    |
+ |   groupOffset[0]   |1     2|           |       |groupOffset[0]      |
+ |                    | tile  |           |       |                    |
+ |                    |3     4| ------->  |       |                    |
+ |                    +-------+           |       V                    |
+ |                            |           |       +-------+            |
+ |                            |           |       |1     3|            |
+ |                            |           |       | tile  |            |
+ |                            |           |       |2     4|            |
+ +----------------------------+           +-------+-------+------------+
+                                           <----->
+                                           groupOffset[1]
+
+Within a tile, each work item is assigned to a single value:
+
+InTile:               OutTile:
++------------+        +------------+
+|   local  ^ |        |  local^    |
+|   Id[1]  | |        |  Id[0]|    |
+|          V |------->|       |    |
+|<-------->* |        |       V    |
+|localId[0]  |        |<----->*    |
++------------+        +------------+
+                      localId[1]
+
 */
 
 #include <iostream>
@@ -40,7 +75,7 @@ int main() {
               << q.get_device().get_info<sycl::info::device::name>() << "\n";
 
     auto globalRange = sycl::range{N, N};
-    auto localRange = sycl::range{32, 32};
+    auto localRange = sycl::range{16, 16};
     auto ndRange = sycl::nd_range{globalRange, localRange};
 
     {
@@ -76,18 +111,22 @@ int main() {
               cgh.parallel_for(ndRange, [=](sycl::nd_item<2> item) {
                 auto globalId = item.get_global_id();
                 auto localId = item.get_local_id();
-                auto localIdTranspose = sycl::id{localId[1], localId[0]};
-                auto wg = item.get_group();
-                auto groupId = wg.get_group_id();
-                auto groupOffsetTranspose =
-                    sycl::range{groupId[1], groupId[0]} * localRange;
+                auto localId_T = sycl::range{localId[1], localId[0]};
+                auto groupOffset = globalId - localId;
+                auto groupOffset_T =
+                    sycl::range{groupOffset[1], groupOffset[0]};
 
-                localAcc[localId] = inAcc[globalId];
+                // Read from global memory in row major and write to local 
+                // memory in column major 
+                localAcc[localId_T] = inAcc[globalId];
 
-                sycl::group_barrier(wg);
+                // We need to wait here to ensure that all work items have
+                // written to local memory before we start reading from it.
+                sycl::group_barrier(item.get_group());
 
-                outAcc[groupOffsetTranspose + localId] =
-                    localAcc[localIdTranspose];
+                // Read from local memory in row major and write to global 
+                // memory in row major fashion
+                outAcc[groupOffset_T + localId] = localAcc[localId];
               });
             });
             q.wait_and_throw();
