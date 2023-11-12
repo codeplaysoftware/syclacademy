882aa408755663936a0f1c582d2bc7ed54b034ea
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2023-11-12 16:04:51 -0700

    Changes to lesson and exercise 6 (#318)
    * 06 Async change
    
    * Update example
    
    * go back to defaultQueue

--- Code_Exercises/Exercise_05_Vector_Add/solution.cpp
+++ Code_Exercises/Exercise_05_Vector_Add/solution.cpp
@@ -10,46 +10,27 @@
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
-
 #include <sycl/sycl.hpp>
 
-class vector_add;
-
-TEST_CASE("vector_add", "vector_add_solution") {
+TEST_CASE("vector_add", "synchronization_buffer_acc") {
   constexpr size_t dataSize = 1024;
 
-  float a[dataSize], b[dataSize], r[dataSize];
-  for (int i = 0; i < dataSize; ++i) {
-    a[i] = static_cast<float>(i);
-    b[i] = static_cast<float>(i);
-    r[i] = 0.0f;
-  }
+  int a[dataSize];
+  sycl::queue defaultQueue;
 
   try {
-    auto defaultQueue = sycl::queue{};
-
     auto bufA = sycl::buffer{a, sycl::range{dataSize}};
-    auto bufB = sycl::buffer{b, sycl::range{dataSize}};
-    auto bufR = sycl::buffer{r, sycl::range{dataSize}};
-
-    defaultQueue
-        .submit([&](sycl::handler& cgh) {
-          sycl::accessor accA{bufA, cgh, sycl::read_only};
-          sycl::accessor accB{bufB, cgh, sycl::read_only};
-          sycl::accessor accR{bufR, cgh, sycl::write_only};
-
-          cgh.parallel_for<vector_add>(
+    defaultQueue.submit([&](sycl::handler& cgh) {
+          sycl::accessor accA{bufA, cgh, sycl::write_only};
+          cgh.parallel_for(
               sycl::range{dataSize},
-              [=](sycl::id<1> idx) { accR[idx] = accA[idx] + accB[idx]; });
+              [=](sycl::id<1> idx) { accA[idx] = 42});
         })
-        .wait();
-
-    defaultQueue.throw_asynchronous();
   } catch (const sycl::exception& e) {
     std::cout << "Exception caught: " << e.what() << std::endl;
-  }
+   }
 
   for (int i = 0; i < dataSize; ++i) {
-    REQUIRE(r[i] == static_cast<float>(i) * 2.0f);
+    REQUIRE(a[i] == 42);
   }
 }
