536ab04280411e1d313082fd959d77805e2cd2b9
Hugh Delaney <hugh.delaney@codeplay.com>
2024-04-05 14:13:55 +0100

    Refactor In_order benchmark (#342)
    * Split some helpers into a helpers file.
    * Apply consistent formatting and naming of vars
    * Add some descriptions of what the benchmark aims to demonstrate.
    * Add cmake.
    
    Co-authored-by: Duncan McBain <duncan.m.mcbain@googlemail.com>

--- Code_Exercises/In_Order_Queue/solution_queue_benchmarking.cpp
+++ Code_Exercises/In_Order_Queue/solution_queue_benchmarking.cpp
@@ -1,101 +1,112 @@
+/*
+ SYCL Academy (c)
+
+ SYCL Academy is licensed under a Creative Commons
+ Attribution-ShareAlike 4.0 International License.
+
+ You should have received a copy of the license along with this
+ work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+*/
+
+// Runs a simple microbenchmark to compare dispatching a non parallel task to:
+//
+// 1. An in order queue
+// 2. An out of order queue
+// 3. Multiple in order queues
+//
+// This microbenchmark aims to demonstrate that weakly parallel, non dependent
+// tasks can benefit from being dispatched concurrently.
+//
+// Caveat: In general if the programmer has many tasks to dispatch, in SYCL it
+// is best to try to encapsulate the logic into as few kernels as possible. The
+// work done by `numIters` `single_task`s here would be better encapsulated in
+// a single `parallel_for` launch with range `numIters`.
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 #include <sycl/sycl.hpp>
 
-#define MAD_4(x, y)                                                                                \
-  x = y * x + y;                                                                                   \
-  y = x * y + x;                                                                                   \
-  x = y * x + y;                                                                                   \
-  y = x * y + x;
-#define MAD_16(x, y)                                                                               \
-  MAD_4(x, y);                                                                                     \
-  MAD_4(x, y);                                                                                     \
-  MAD_4(x, y);                                                                                     \
-  MAD_4(x, y);
-#define MAD_64(x, y)                                                                               \
-  MAD_16(x, y);                                                                                    \
-  MAD_16(x, y);                                                                                    \
-  MAD_16(x, y);                                                                                    \
-  MAD_16(x, y);
+#include "queue_benchmarking_helpers.hpp"
 
-template <class T> static T busy_sleep(size_t N, T i) {
-  T x = 1.3f;
-  T y = i;
-  for (size_t j = 0; j < N; j++) {
-    MAD_64(x, y);
+using T = float;
+
+constexpr int numIters = 100;
+
+// Run busy_sleep numKernels times on a single thread using single_task
+template <typename T> auto bench(sycl::queue q, int numKernels) {
+  auto *out = sycl::malloc_device<T>(numKernels, q);
+
+  auto s = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < numKernels; i++) {
+    q.single_task([=]() { out[i] = busy_sleep<T>(2E5, i); });
   }
-  return y;
-}
-
-auto bench(sycl::queue Q, int N_kernel) {
-  auto *out = sycl::malloc_device<double>(N_kernel, Q);
-
-  const auto s = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < N_kernel; i++) {
-    Q.single_task([=]() { out[i] = busy_sleep<float>(2E5, i); });
-  }
-  Q.wait();
-  const auto e = std::chrono::high_resolution_clock::now();
+  q.wait();
+  auto e = std::chrono::high_resolution_clock::now();
   return std::chrono::duration_cast<std::chrono::microseconds>(e - s).count();
 }
 
-auto bench_multiple_queues(std::vector<sycl::queue> Qs, int N_kernel_per_queue) {
+// Run busy_sleep numKernels times on multiple in order queues using
+// single_task
+template <typename T>
+auto bench_multiple_queues(std::vector<sycl::queue> qs,
+                           int numKernelsPerQueue) {
 
-  auto *out = sycl::malloc_device<double>(N_kernel_per_queue * Qs.size(), Qs[0]);
+  auto *out = sycl::malloc_device<T>(numKernelsPerQueue * qs.size(), qs[0]);
 
-  const auto s = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < N_kernel_per_queue; i++) {
-    for (auto Q : Qs) {
-      Q.single_task([=]() { out[i] = busy_wait<double>(1E5, i); });
+  auto s = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < numKernelsPerQueue; i++) {
+    for (auto q : qs) {
+      q.single_task([=]() { out[i] = busy_sleep<T>(1E5, i); });
     }
   }
-  for (auto Q : Qs) {
-    Q.wait();
+  for (auto q : qs) {
+    q.wait();
   }
-  const auto e = std::chrono::high_resolution_clock::now();
+  auto e = std::chrono::high_resolution_clock::now();
   return std::chrono::duration_cast<std::chrono::microseconds>(e - s).count();
 }
 
-TEST_CASE("in_order_queue", "in_order_slow") {
- sycl::queue Q{sycl::property::queue::in_order{}};
- bench(Q, 1); // Warmup
+void run_single_queue(sycl::queue q) {
+  bench<T>(q, 1); // Warmup
 
- const int N = 100;
- auto kernels_1 =  bench(Q, 1);
- std::cout << "1" << " kernels took: " << kernels_1 << "ms" << std::endl;
- auto kernels_N =  bench(Q, N);
- std::cout << N << " kernels took: " <<kernels_N << "ms" << std::endl;
- std::cout << "Ratio N/1: " << kernels_N / kernels_1 << std::endl;
+  auto singleKernelTime = bench<T>(q, 1);
+  auto nKernelsTime = bench<T>(q, numIters);
+  std::cout << "1 kernel took: " << singleKernelTime << "ms" << std::endl;
+  std::cout << numIters << " kernels took: " << nKernelsTime << "ms"
+            << std::endl;
+  std::cout << "Ratio N/1: "
+            << static_cast<float>(nKernelsTime) / singleKernelTime << std::endl
+            << std::endl;
 }
 
-TEST_CASE("in_order_queue", "out_of_order") {
- sycl::queue Q;
- bench(Q, 1); // Warmup
-
- const int N = 100;
- auto kernels_1 =  bench(Q, 1);
- std::cout << "1" << " kernels took: " << kernels_1 << "ms" << std::endl;
- auto kernels_N =  bench(Q, N);
- std::cout << N << " kernels took: " <<kernels_N << "ms" << std::endl;
- std::cout << "Ratio N/1: " << kernels_N / kernels_1 << std::endl;
+TEST_CASE("in_order_slow", "in_order_queue") {
+  sycl::queue q{sycl::property::queue::in_order{}};
+  run_single_queue(q);
 }
 
-
-TEST_CASE("in_order_queue", "in_order_fast") {
-
- const int N_kernel_per_queue = 10;
- const int N_queues = 10;
-
- std::vector<sycl::queue> Qs;
- for (int i=0; i<N_queues; i++) {
-   sycl::queue Q{sycl::property::queue::in_order{}};
-   Qs.push_back(Q);
- }
- bench_multiple_queues({Q[0]},1); // Trigger JITing
- 
- auto kernels_1 = bench_multiple_queues({Q[0]},1);
- std::cout << "1" << " kernels took: " << kernels_1 << "ms" << std::endl;
-
- auto kernels_N = bench_multiple_queues(Qs, N_queues);
- std::cout << N << " in-order_fast kernels took: " <<kernels_N << "ms" << std::endl;
- std::cout << "Ratio N/1: " << kernels_N / kernels_1 << std::endl;
+TEST_CASE("out_of_order", "in_order_queue") {
+  sycl::queue q;
+  run_single_queue(q);
 }
 
+TEST_CASE("multiple_in_order_queues", "in_order_queue") {
+
+  constexpr int numKernelsPerQueue = 10;
+  constexpr int numQueues = numIters / numKernelsPerQueue;
+
+  std::vector<sycl::queue> qs;
+  for (int i = 0; i < numQueues; i++) {
+    sycl::queue q{sycl::property::queue::in_order{}};
+    qs.push_back(q);
+  }
+  bench_multiple_queues<T>({qs[0]}, 1); // Warmup
+
+  auto singleKernelTime = bench<T>({qs[0]}, 1);
+  auto nKernelsTime = bench_multiple_queues<T>(qs, numQueues);
+  std::cout << "1 kernel took: " << singleKernelTime << "ms" << std::endl;
+  std::cout << numIters << " in-order kernels took: " << nKernelsTime
+            << "ms" << std::endl;
+  std::cout << "Ratio N/1: "
+            << static_cast<float>(nKernelsTime) / singleKernelTime << std::endl
+            << std::endl;
+}
--- Code_Exercises/In_Order_Queue/source_queue_benchmarking.cpp
+++ Code_Exercises/In_Order_Queue/source_queue_benchmarking.cpp
@@ -1,50 +1,60 @@
+/*
+ SYCL Academy (c)
+
+ SYCL Academy is licensed under a Creative Commons
+ Attribution-ShareAlike 4.0 International License.
+
+ You should have received a copy of the license along with this
+ work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+*/
+
+// Runs a simple microbenchmark to compare dispatching a non parallel task to:
+//
+// 1. An in order queue
+//
+// TODO: Check how this compares to an out of order queue
+//
+// This microbenchmark aims to demonstrate that weakly parallel, non dependent
+// tasks can benefit from being dispatched concurrently.
+//
+// Caveat: In general if the programmer has many tasks to dispatch, in SYCL it
+// is best to try to encapsulate the logic into as few kernels as possible. The
+// work done by `numIters` `single_task`s here would be better encapsulated in
+// a single `parallel_for` launch with range `numIters`.
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 #include <sycl/sycl.hpp>
 
-#define MAD_4(x, y)                                                                                \
-  x = y * x + y;                                                                                   \
-  y = x * y + x;                                                                                   \
-  x = y * x + y;                                                                                   \
-  y = x * y + x;
-#define MAD_16(x, y)                                                                               \
-  MAD_4(x, y);                                                                                     \
-  MAD_4(x, y);                                                                                     \
-  MAD_4(x, y);                                                                                     \
-  MAD_4(x, y);
-#define MAD_64(x, y)                                                                               \
-  MAD_16(x, y);                                                                                    \
-  MAD_16(x, y);                                                                                    \
-  MAD_16(x, y);                                                                                    \
-  MAD_16(x, y);
+#include "queue_benchmarking_helpers.hpp"
 
-template <class T> static T busy_sleep(size_t N, T i) {
-  T x = 1.3f;
-  T y = i;
-  for (size_t j = 0; j < N; j++) {
-    MAD_64(x, y);
+using T = float;
+
+constexpr int numIters = 100;
+
+// Run busy_sleep numKernels times on a single thread using single_task
+template <typename T> auto bench(sycl::queue q, int numKernels) {
+  auto *out = sycl::malloc_device<T>(numKernels, q);
+
+  auto s = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < numKernels; i++) {
+    q.single_task([=]() { out[i] = busy_sleep<T>(2E5, i); });
   }
-  return y;
-}
-
-auto bench(sycl::queue Q, int N_kernel) {
-  auto *out = sycl::malloc_device<double>(N_kernel, Q);
-
-  const auto s = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < N_kernel; i++) {
-    Q.single_task([=]() { out[i] = busy_sleep<float>(2E5, i); });
-  }
-  Q.wait();
-  const auto e = std::chrono::high_resolution_clock::now();
+  q.wait();
+  auto e = std::chrono::high_resolution_clock::now();
   return std::chrono::duration_cast<std::chrono::microseconds>(e - s).count();
 }
 
-TEST_CASE("in_order_queue", "in_order_slow") {
- sycl::queue Q{sycl::property::queue::in_order{}};
- bench(Q, 1); // Warmup
+TEST_CASE("in_order_slow", "in_order_queue") {
+  sycl::queue q{sycl::property::queue::in_order{}};
+  bench<T>(q, 1); // Warmup
 
- const int N = 100;
- auto kernels_1 =  bench(Q, 1);
- std::cout << "1" << " kernels took: " << kernels_1 << "ms" << std::endl;
- auto kernels_N =  bench(Q, N);
- std::cout << N << " kernels took: " <<kernels_N << "ms" << std::endl;
- std::cout << "Ratio N/1: " << kernels_N / kernels_1 << std::endl;
+  auto singleKernelTime = bench<T>(q, 1);
+  auto nKernelsTime = bench<T>(q, numIters);
+  std::cout << "1 kernel took: " << singleKernelTime << "ms" << std::endl;
+  std::cout << numIters << " kernels took: " << nKernelsTime << "ms"
+            << std::endl;
+  std::cout << "Ratio N/1: "
+            << static_cast<float>(nKernelsTime) / singleKernelTime << std::endl
+            << std::endl;
 }
