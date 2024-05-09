a61c6da90d75808074d5b9ee74ae9260d2cec7cd
Rod Burns <800194+rodburns@users.noreply.github.com>
2024-05-09 14:17:14 +0100

    Update solution.cpp

--- Code_Exercises/What_is_SYCL/solution.cpp
+++ Code_Exercises/What_is_SYCL/solution.cpp
@@ -13,7 +13,43 @@
 
 #include <sycl/sycl.hpp>
 
-// The below tests that the header file has been included
-TEST_CASE("empty_sycl_source_file", "compiling_with_sycl_solution") {
-  REQUIRE(true);
+class vector_add;
+
+TEST_CASE("vector_add", "vector_add_solution") {
+  constexpr size_t dataSize = 1024;
+
+  float a[dataSize], b[dataSize], r[dataSize];
+  for (int i = 0; i < dataSize; ++i) {
+    a[i] = static_cast<float>(i);
+    b[i] = static_cast<float>(i);
+    r[i] = 0.0f;
+  }
+
+  try {
+    auto defaultQueue = sycl::queue{};
+
+    auto bufA = sycl::buffer{a, sycl::range{dataSize}};
+    auto bufB = sycl::buffer{b, sycl::range{dataSize}};
+    auto bufR = sycl::buffer{r, sycl::range{dataSize}};
+
+    defaultQueue
+        .submit([&](sycl::handler& cgh) {
+          sycl::accessor accA{bufA, cgh, sycl::read_only};
+          sycl::accessor accB{bufB, cgh, sycl::read_only};
+          sycl::accessor accR{bufR, cgh, sycl::write_only};
+
+          cgh.parallel_for<vector_add>(
+              sycl::range{dataSize},
+              [=](sycl::id<1> idx) { accR[idx] = accA[idx] + accB[idx]; });
+        })
+        .wait();
+
+    defaultQueue.throw_asynchronous();
+  } catch (const sycl::exception& e) {
+    std::cout << "Exception caught: " << e.what() << std::endl;
+  }
+
+  for (int i = 0; i < dataSize; ++i) {
+    REQUIRE(r[i] == static_cast<float>(i) * 2.0f);
+  }
 }
