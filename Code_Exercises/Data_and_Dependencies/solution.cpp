c8d10720ca610a8c8f31fc92c96a1d0c24d22405
Arjen Tamerus <at748@cam.ac.uk>
2025-06-06 14:45:37 +0100

    Update Data nd Dependencies for ISC25
     - Remove USM material
     - Add extra slide about buffer accessor modes

--- Code_Exercises/Data_and_Dependencies/solution.cpp
+++ Code_Exercises/Data_and_Dependencies/solution.cpp
@@ -21,14 +21,6 @@ class kernel_b_2;
 class kernel_c_2;
 class kernel_d_2;
 
-int usm_selector(const sycl::device& dev) {
-  if (dev.has(sycl::aspect::usm_device_allocations)) {
-    if (dev.has(sycl::aspect::gpu)) return 2;
-    return 1;
-  }
-  return -1;
-}
-
 void test_buffer() {
   constexpr size_t dataSize = 1024;
 
@@ -92,72 +84,6 @@ void test_buffer() {
   SYCLACADEMY_ASSERT_EQUAL(out, [](size_t i) { return i * 2.0f; });
 }
 
-void test_usm() {
-  constexpr size_t dataSize = 1024;
-
-  int inA[dataSize], inB[dataSize], inC[dataSize], out[dataSize];
-  for (int i = 0; i < dataSize; ++i) {
-    inA[i] = static_cast<float>(i);
-    inB[i] = static_cast<float>(i);
-    inC[i] = static_cast<float>(i);
-    out[i] = 0.0f;
-  }
-
-  try {
-    auto usmQueue = sycl::queue{usm_selector};
-
-    auto devicePtrInA = sycl::malloc_device<float>(dataSize, usmQueue);
-    auto devicePtrInB = sycl::malloc_device<float>(dataSize, usmQueue);
-    auto devicePtrInC = sycl::malloc_device<float>(dataSize, usmQueue);
-    auto devicePtrOut = sycl::malloc_device<float>(dataSize, usmQueue);
-
-    auto e1 = usmQueue.memcpy(devicePtrInA, inA, sizeof(float) * dataSize);
-    auto e2 = usmQueue.memcpy(devicePtrInB, inB, sizeof(float) * dataSize);
-    auto e3 = usmQueue.memcpy(devicePtrInC, inC, sizeof(float) * dataSize);
-
-    auto e4 = usmQueue.parallel_for<kernel_a_2>(
-        sycl::range{dataSize}, e1, [=](sycl::id<1> idx) {
-          auto globalId = idx[0];
-          devicePtrInA[globalId] = devicePtrInA[globalId] * 2.0f;
-        });
-
-    auto e5 = usmQueue.parallel_for<kernel_b_2>(
-        sycl::range{dataSize}, {e2, e4}, [=](sycl::id<1> idx) {
-          auto globalId = idx[0];
-          devicePtrInB[globalId] += devicePtrInA[globalId];
-        });
-
-    auto e6 = usmQueue.parallel_for<kernel_c_2>(
-        sycl::range{dataSize}, {e3, e4}, [=](sycl::id<1> idx) {
-          auto globalId = idx[0];
-          devicePtrInC[globalId] -= devicePtrInA[globalId];
-        });
-
-    auto e7 = usmQueue.parallel_for<kernel_d_2>(
-        sycl::range{dataSize}, {e5, e6}, [=](sycl::id<1> idx) {
-          auto globalId = idx[0];
-          devicePtrOut[globalId] =
-              devicePtrInB[globalId] + devicePtrInC[globalId];
-        });
-
-    auto e8 = usmQueue.memcpy(out, devicePtrOut, sizeof(float) * dataSize, e7);
-
-    e8.wait();
-
-    sycl::free(devicePtrInA, usmQueue);
-    sycl::free(devicePtrInB, usmQueue);
-    sycl::free(devicePtrInC, usmQueue);
-    sycl::free(devicePtrOut, usmQueue);
-
-    usmQueue.throw_asynchronous();
-  } catch (const sycl::exception& e) {
-    std::cout << "Exception caught: " << e.what() << std::endl;
-  }
-
-  SYCLACADEMY_ASSERT_EQUAL(out, [](size_t i) { return i * 2.0f; });
-}
-
 int main() {
   test_buffer();
-  test_usm();
 }
